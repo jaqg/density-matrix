7caf22a7da2abaf1b26b69e355c8a2dd6129545f
José Antonio Quiñonero Gris <jose.aqg.universidad@gmail.com>
2024-05-16 00:21:41 +0200

    Added introduction to the document

---
+++ scans/program/modules/IO.f90
@@ -0,0 +1,39 @@
+! +--------------------------------------------+
+! | Author: Jose Antonio Quinonero Gris        |
+! | Creation date: Tuesday 17:27:38 30/04/2024 |
+! +--------------------------------------------+
+module io 
+    ! 
+    ! Input/Output module
+    !
+    use declarations
+    use strings
+    !
+    implicit none
+    !
+    contains
+
+    ! subroutine open_files
+    !     implicit none
+    !
+    !     !
+    !     return
+    ! end subroutine open_files 
+
+    ! subroutine print_input(ouf)
+    !     implicit none
+    !     integer, intent(in) :: ouf
+    !
+    !
+    !     return
+    ! end subroutine print_input 
+
+    ! subroutine print_output(ouf)
+    !     implicit none
+    !     integer, intent(in) :: ouf
+    !
+    !     write(ouf,*)
+    !
+    !     return
+    ! end subroutine print_output 
+end module io 
---
+++ scans/program/modules/density_matrices.f90
@@ -0,0 +1,16 @@
+! +--------------------------------------------+
+! | Author: Jose Antonio Quinonero Gris        |
+! | Creation date: Tuesday 17:30:52 30/04/2024 |
+! +--------------------------------------------+
+module density_matrices
+    ! 
+    ! Module to contain density matrices procedures
+    !
+    use declarations
+    use strings
+    !
+    implicit none
+    !
+    contains
+
+end module density_matrices 
